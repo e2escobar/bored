b99a04b80de6bbcda773baa166f6553debce7aad
Eduardo Escobar <edu@ghost.local>
2022-11-22 22:02:33 -0500

    Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import Button from "./components/Button"
+
 function App() {
+
+  const otraCosa = () => {
+    console.log('click algo')
+  }
+
+  const subirCambios = () => {
+    console.log('subir cambios ⬆️')
+  }
 
   return (
     <div className="App container">
-      <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+      <Button className="btn btn-primary" onClick={otraCosa} primary>Primary</Button>
+      <Button className="btn btn-outline btn-warning" onClick={subirCambios} secondary>Daniela</Button>
     </div>
   )
 }
